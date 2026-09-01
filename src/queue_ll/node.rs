bc116f41349bb453fc16a21949e9289aac5f1849
Big Smarty <budaniasco@gmail.com>
2026-09-01 17:32:07 +0200

    add sound next method to Node

--- src/queue_ll/node.rs
+++ src/queue_ll/node.rs
@@ -1,6 +1,12 @@
-use std::sync::atomic::AtomicPtr;
+use std::sync::atomic::{AtomicPtr, Ordering};
 
 pub struct Node<T> {
     pub data: T,
     pub next: AtomicPtr<Self>,
+}
+
+impl<T> Node<T> {
+    pub unsafe fn next(node: *mut Self, ordering: Ordering) -> *mut Self {
+        unsafe { (*node).next.load(ordering) }
+    }
 }
